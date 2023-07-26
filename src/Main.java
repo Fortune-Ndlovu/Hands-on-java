19381e048a11966075e5c4648c5278b3a46d84c9
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-26 16:34:53 +0100

    Concatinating

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
         double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
         String studentFirstName = "Mary";
         String studentLastName = "Doe";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(2);
 
-        System.out.println("studentAge: " + studentAge);
-        System.out.println("studentGPA: " + studentGPA);
-        System.out.println("hasPerfectAttendance: " + hasPerfectAttendance);
-        System.out.println("studentFirstName: " + studentFirstName);
-        System.out.println("studentLastName: " + studentLastName);
-        System.out.println("studentFirstInitial: " + studentFirstInitial);
-        System.out.println("studentLastInitial: " + studentLastInitial);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
     }
 }
