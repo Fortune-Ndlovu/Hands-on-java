4a404d118cd1db05d18cf5e39400e4e33939725d
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-26 21:59:57 +0100

    Tinkering with multi choice variables and user input

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isOnRepeat = true;
+       String question = "Do you like pies, cakes, or scones?";
+       String choiceOne = "pies";
+       String choiceTwo = "cakes";
+       String choiceThree = "scones";
 
-        while (isOnRepeat) {
-            System.out.println("Playing current song");
-            System.out.println("Would you like to take this song off of repeat? if so, answer yes");
-            String userInput = input.next();
+        // Write a print statement asking the question
+        System.out.println(question);
 
-            if(userInput.equals("yes")) {
-                isOnRepeat = false;
-            }
+        // Write a print statement giving the answer choices
+        System.out.println("Please pick one " + choiceOne +
+        " or " + choiceTwo + " or " + choiceThree +
+        " but it would make me happy if you picked " + choiceOne);
+
+        // Have the user input an answer
+        Scanner answer = new Scanner(System.in);
+        String userInput = answer.next();
+        
+        if (userInput.equals("pies")) {
+            System.out.println("Yeyy you made me happy!");
+        } else {
+            System.out.println("No you made me sad :(");
         }
-        System.out.println("Playing next song");
     }
 }
