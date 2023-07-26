7adb3f4a38cfdbbaa3833c8a8c6f44a0001ffc12
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-26 16:19:28 +0100

    Exploring various data types such as int, double, char, and boolean, creating relevant variable with respected values and logging out their results via terminal.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World Fortune from EARTH");
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println("studentAge: " + studentAge);
+        System.out.println("studentGPA: " + studentGPA);
+        System.out.println("studentFirstInitial: " + studentFirstInitial);
+        System.out.println("studentLastInitial: " + studentLastInitial);
+        System.out.println("hasPerfectAttendance: " + hasPerfectAttendance);
     }
 }
