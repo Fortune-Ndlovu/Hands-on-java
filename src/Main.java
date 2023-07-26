08b7ca9d37ae1857d50614aa641c39daec2b528a
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-26 16:25:23 +0100

    remove data types

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
         String studentFirstName = "Mary";
         String studentLastName = "Doe";
 
+
         System.out.println("studentAge: " + studentAge);
         System.out.println("studentGPA: " + studentGPA);
-        System.out.println("studentFirstInitial: " + studentFirstInitial);
-        System.out.println("studentLastInitial: " + studentLastInitial);
         System.out.println("hasPerfectAttendance: " + hasPerfectAttendance);
         System.out.println("studentFirstName: " + studentFirstName);
         System.out.println("studentLastName: " + studentLastName);
