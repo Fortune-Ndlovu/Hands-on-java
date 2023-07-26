980a1bc12e227262e3eb0e8059629a88271ce785
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-26 21:30:17 +0100

    Implemented logic of when checking while the user is playing a song on repeat, play a current song and they have the option to take it on repeat and play the next song

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Pick a number between 1 and 10");
-       Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
 
-       int inputtedNum = scanner.nextInt();
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? if so, answer yes");
+            String userInput = input.next();
 
-       if (inputtedNum < 5) {
-            System.out.println("Enjoy the good luck a friend gives you");
-       } else {
-        System.out.println("Your shoe selected will make you happy today");
-       }
+            if(userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song");
     }
 }
