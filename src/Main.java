f8c43fc3776a3d4034876d76f3b4ac36d198a655
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-26 16:24:10 +0100

    Added string primitive type

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,16 @@ public class Main {
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Mary";
+        String studentLastName = "Doe";
 
         System.out.println("studentAge: " + studentAge);
         System.out.println("studentGPA: " + studentGPA);
         System.out.println("studentFirstInitial: " + studentFirstInitial);
         System.out.println("studentLastInitial: " + studentLastInitial);
         System.out.println("hasPerfectAttendance: " + hasPerfectAttendance);
+        System.out.println("studentFirstName: " + studentFirstName);
+        System.out.println("studentLastName: " + studentLastName);
+
     }
 }
